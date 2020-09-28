ae30c53fb2e1449c47c6bb11346e48905436b0d4
Bethwelkip <bkiplimo@princeton.edu>
2020-09-28 06:57:23 -0400

    add quotes and quote objects array

---
+++ src/app/quote.ts
@@ -0,0 +1,12 @@
+export class Quote {
+    //  describe: boolean;
+    id: number;
+    name: string;
+    quote: string;
+    author: string;
+
+    //constructor(id: number, name: string, quote: string, author: string) {
+    // this.describe = false;
+
+    //}
+}
