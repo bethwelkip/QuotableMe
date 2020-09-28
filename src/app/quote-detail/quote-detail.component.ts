bb17e09899adfdfa4a8ba96a29e6ccb4488f8dcb
Bethwelkip <bkiplimo@princeton.edu>
2020-09-28 10:01:02 -0400

    add event emitters to delete quotes

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Quote } from '../quote'
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-detail',
@@ -8,7 +8,21 @@ import { Quote } from '../quote'
 })
 export class QuoteDetailComponent implements OnInit {
   @Input() quote: Quote;
-  // @Output()  submit = new EventEmitter
+  // @Output() submit = new EventEmitter<boolean>();
+  @Output() upVote = new EventEmitter<boolean>();
+  @Output() downVote = new EventEmitter<boolean>()
+  @Output() quotesDelete = new EventEmitter<boolean>();
+
+  quoteDelete(deletes: boolean) {
+    this.quotesDelete.emit(deletes);
+  }
+  quoteUpvote(upvote: boolean) {
+    this.upVote.emit(upvote);
+  }
+  quoteDownvote(downvote: boolean) {
+    this.downVote.emit(downvote);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
