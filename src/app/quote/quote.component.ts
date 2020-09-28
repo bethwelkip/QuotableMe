e21e339a60c8b3e394396e9ac93b2c5abf6cf4f9
Bethwelkip <bkiplimo@princeton.edu>
2020-09-28 08:39:05 -0400

    rectify quote authors

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,19 +8,25 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    { id: 1, name: 'Bethwel', quote: 'Love is a scandal of the personal sort.', author: 'Chekhov' },
-    {
-      id: 2, name: 'Bethwel', quote: 'Man\'s life is brief, but yet there is no man who cannot boast\
-    that there have been terrible moments in his past.', author: 'Chekhov'
-    },
-    { id: 3, name: 'Bethwel', quote: "You can't have it all, all at once.", author: 'Ruth Ginsberg' },
-    { id: 4, name: 'Bethwel', quote: "All happy families are alike; each unhappy family is unhappy in its own way.", author: ' Leo Tolstoy , Anna Karenina' } /*
+    // { id: 1, name: 'Bethwel', quote: 'Love is a scandal of the personal sort.', author: 'Chekhov' },
+    // {
+    //   id: 2, name: 'Bethwel', quote: 'Man\'s life is brief, but yet there is no man who cannot boast\
+    // that there have been terrible moments in his past.', author: 'Chekhov'
+    // },
+    // { id: 3, name: 'Bethwel', quote: "You can't have it all, all at once.", author: 'Ruth Ginsberg' },
+    // { id: 4, name: 'Bethwel', quote: "All happy families are alike; each unhappy family is unhappy in its own way.", author: ' Leo Tolstoy , Anna Karenina' } 
+    // 
     new Quote(1, 'Bethwel', 'Love is a scandal of the personal sort.', 'Chekhov'),
     new Quote(2, 'Bethwel', 'Man\'s life is brief, but yet there is no man who cannot boast\
      that there have been terrible moments in his past.', 'Chekhov'),
-    new Quote(3, 'Bethwel', "You can't have it all, all at once.", 'Chekhov'),
-    new Quote(4, 'Bethwel', "All happy families are alike; each unhappy family is unhappy in its own way.", ' Leo Tolstoy , Anna Karenina') */
+    new Quote(3, 'Bethwel', "You can't have it all, all at once.", 'Ruth Ginsberg'),
+    new Quote(4, 'Bethwel', "All happy families are alike; each unhappy family is unhappy in its own way.", ' Leo Tolstoy , Anna Karenina')
+
   ];
+  showDetails(index) {
+    this.quotes[index].describe = !this.quotes[index].describe;
+
+  }
 
   constructor() { }
 
