978ab3b2330d8475df8216a1d2b3fe1802aa5e6f
Bethwelkip <bkiplimo@princeton.edu>
2020-09-29 01:50:50 -0400

    add upvote and downvote initialization

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,12 +1,16 @@
 export class Quote {
     describe: boolean;
+    upvote: number;
+    downvote: number;
     // id: number;
     // name: string;
     // quote: string;
     // author: string;
 
-    constructor(public id: number, public name: string, public quote: string, public author: string) {
+    constructor(public id: number, public name: string, public quote: string, public author: string, public create: Date) {
         this.describe = false;
+        this.upvote = 0;
+        this.downvote = 0;
 
     }
 }
