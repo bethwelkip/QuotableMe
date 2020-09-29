31462f18e6d45fb0ac11308d8d8d9f040771da5f
Bethwelkip <bkiplimo@princeton.edu>
2020-09-29 01:52:21 -0400

    add ability  to upvote, downvote and add new quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -16,11 +16,11 @@ export class QuoteComponent implements OnInit {
     // { id: 3, name: 'Bethwel', quote: "You can't have it all, all at once.", author: 'Ruth Ginsberg' },
     // { id: 4, name: 'Bethwel', quote: "All happy families are alike; each unhappy family is unhappy in its own way.", author: ' Leo Tolstoy , Anna Karenina' } 
     // 
-    new Quote(1, 'Bethwel', 'Love is a scandal of the personal sort.', 'Chekhov'),
+    new Quote(1, 'Bethwel', 'Love is a scandal of the personal sort.', 'Chekhov', new Date(2020, 5, 6)),
     new Quote(2, 'Bethwel', 'Man\'s life is brief, but yet there is no man who cannot boast\
-     that there have been terrible moments in his past.', 'Chekhov'),
-    new Quote(3, 'Bethwel', "You can't have it all, all at once.", 'Ruth Ginsberg'),
-    new Quote(4, 'Bethwel', "All happy families are alike; each unhappy family is unhappy in its own way.", ' Leo Tolstoy , Anna Karenina')
+     that there have been terrible moments in his past.', 'Chekhov', new Date(2016, 5, 6)),
+    new Quote(3, 'Bethwel', "You can't have it all, all at once.", 'Ruth Ginsberg', new Date(2019, 5, 6)),
+    new Quote(4, 'Bethwel', "All happy families are alike; each unhappy family is unhappy in its own way.", ' Leo Tolstoy , Anna Karenina', new Date(2018, 5, 6))
 
   ];
   showDetails(index) {
@@ -36,14 +36,21 @@ export class QuoteComponent implements OnInit {
     console.log(index);
     if (upvote) {
 
+      this.quotes[index].upvote += 1;
 
     }
   }
   downVote(downvote, index) {
     console.log(index);
     if (downvote) {
+      this.quotes[index].downvote += 1;
 
     }
+  }
+  addQuote(quot) {
+    let len = this.quotes.length;
+    quot.id = len + 1;
+    this.quotes.push(quot);
   }
   constructor() { }
 
